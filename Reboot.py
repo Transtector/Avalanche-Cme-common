cfe228a9e12ee302dbd81e1916c2add6b9de30bb
James Brunner <james.brunner@smithsmicrowave.com>
2017-04-17 18:16:31 -0700

    Remove the debug “print” statements (logging accommodates these better)
    Add back the power_off argument in _reboot as it’s needed for the command selection

--- Reboot.py
+++ Reboot.py
@@ -32,21 +32,19 @@ def restart(power_off=False, recovery_mode=False, factory_reset=False, logger=No
 
 	if factory_reset and settings_file and os.path.isfile(settings_file):
 		try:
-			print("[DEBUG] Reboot.py, 35: Remove settings file: {0}".format(settings_file))
 			#os.remove(settings_file)
 			if logger:
 				logger.info("CME user settings file removed")
+
 		except Exception as e:
 			if logger:
 				logger.error("CME failed to remove user settings file, {}".format(e))
 
 		if is_a_cme():
-			print("[DEBUG] Reboot.py, 44: Set DHCP False")
 			#set_dhcp(False)
 			if logger:
 				logger.info("CME DHCP turned OFF - static addressing will be used")
 
-			print("[DEBUG] Reboot.py, 49: Write default net addresses")
 			#write_network_addresses({ 
 			#	'address': '192.168.1.30', 
 			#	'netmask': '255.255.255.0', 
@@ -56,42 +54,36 @@ def restart(power_off=False, recovery_mode=False, factory_reset=False, logger=No
 			#})
 			if logger:
 				logger.info("CME network settings reset to factory defaults")
-
-			print("[DEBUG] Reboot.py, 60: Set default NTP servers")
-			#ntp_servers(['time.nist.gov'])
-			if logger:
-				logger.info("CME NTP servers set to factory defaults")
-
+			
+			ntp_servers(['time.nist.gov'])
 			ntp_enable = ['systemctl', 'enable', 'ntp']
-
 			if is_a_docker():
-				print("[DEBUG] Reboot.py, 68: Enable NTP")
 				#docker_run(ntp_enable)
 			else:
-				print("[DEBUG] Reboot.py, 71: Enable NTP")
 				#subprocess.call(ntp_enable)
 
 			if logger:
+				logger.info("CME NTP servers set to factory defaults")
 				logger.info("CME NTP system enabled")
+
 		
 	if recovery_mode and not os.path.isfile(recovery_file):
-		print("[DEBUG] Reboot.py, 78: Touching recovery file: {0}".format(recovery_file))
 		open(recovery_file, 'w').close()
 		if logger:
 			logger.info("CME set to boot into recovery mode")
 	
 	elif os.path.isfile(recovery_file):
-		print("[DEBUG] Reboot.py, 84: Removing existing recovery file: {0}".format(recovery_file))
 		os.remove(recovery_file)
 
 	if power_off:
-		print("[DEBUG] Reboot.py, 88: Touching poweroff_file: {0}".format(poweroff_file))
 		open(poweroff_file, 'w').close()
+		if logger:
+			logger.info("CME set to power off/standby")
 
-	_reboot(delay, logger)
+	_reboot(delay, power_off, logger)
 
 
-def _reboot(delay=1, logger=None):
+def _reboot(delay=1, power_off=False, logger=None):
 
 	# These commands will cause SIGTERM to be sent to all running processes.
 	# The cleanup() function in Cme-init/__main__.py is a callback listening
@@ -113,6 +105,5 @@ def _reboot(delay=1, logger=None):
 		if is_a_docker():
 			docker_run(command) 
 		else:
-			print("[DEBUG] Reboot.py, 116: reboot/halt now")
 			subprocess.call(command)
 
