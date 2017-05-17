31304da948500827e36e1e438d3c53b7ee861f0a
James Brunner <james.brunner@smithsmicrowave.com>
2017-05-17 11:17:45 -0600

    Moves the user settings under the API class/key.  This prevents problems with Cme-hw app container from trying to recreate USER_SETTINGS when it doesn’t really need to.

--- Config.py
+++ Config.py
@@ -221,6 +221,85 @@ class API:
 	USERNAME = 'admin'
 	PASSHASH = 'b56e0b4ea4962283bee762525c2d490f' # md5('Welcome1')
 
+	from .DictPersistJSON import DictPersistJSON
+	from .ClockUtils import check_ntp, ntp_servers
+	from .IpUtils import mac, dhcp, address, netmask, gateway
+
+	''' User settings are cached in an external file 'settings.json'.  Import
+		the Settings.py module and pass it through this class constructor to
+		update the default values below.  Simply delete the settings.json file
+		to return values to their factory defaults.
+
+		Access USER_SETTINGS like this:
+
+			from .common import Config
+			user_setting = Config.API.USER_SETTINGS['user_setting_key']
+
+		*Note: if you want to save a USER_SETTING that's part of
+		a dict itself (e.g., GENERAL > NAME), then you have to 
+		write the enire object.
+
+		I.e., do this to make changes that get saved to disk:
+
+			general = Config.API.USER_SETTINGS['GENERAL']
+			general['NAME'] = 'My cool new name'
+			Config.API.USER_SETTINGS['GENERAL'] = general
+
+
+		*Note: Use an underscore character on items that you DON'T
+		want to get serialized (e.g., to API respones).  For example
+		the username and passhash are user settings, but we never
+		want to serve them up as part of a response, so we prefix
+		with underscores.
+	'''
+	USER_SETTINGS = DictPersistJSON(PATHS.SETTINGS, {
+
+		'__username': USERNAME,
+		'__passhash': PASSHASH,
+		'__device': INFO.DEVICE,
+
+		'general': {
+			'name': 'CME',
+			'description': 'Core monitoring engine',
+			'location': '',
+			'sitecode': ''
+		},
+
+		'support': {
+			'contact': '',
+			'email': '',
+			'phone': ''
+		},
+
+		'temperature': {
+			'displayUnits': TemperatureUnits.CELSIUS,
+			'warningTemp': 65, # ºC
+			'alarmTemp': 80 # ºC
+		},
+
+		'clock': {
+			'ntp': check_ntp(),
+			'servers': ntp_servers(),
+			'status': [],
+			'zone': 0,
+			'displayRelativeTo': RelativeTo.UTC,
+			'display12Hour': False,
+			'displayDateFormat': "YYYY-MM-DD",
+			'displayTimeFormat24Hour': "HH:mm:ss",
+			'displayTimeFormat12Hour': "h:mm:ss A"
+		},
+
+		'network': {
+			'mac': mac(),
+			'dhcp': dhcp(),
+			'address': address(),
+			'netmask': netmask(),
+			'gateway': gateway(),
+			'primary': '8.8.4.4',
+			'secondary': '8.8.8.8'
+		}
+	})
+
 
 
 class FLASK:
@@ -232,89 +311,5 @@ class FLASK:
 	SESSION_EXPIRATION = API.SESSION_EXPIRATION
 	UPLOAD_FOLDER = PATHS.UPLOADS
 	MAX_CONTENT_LENGTH = UPDATES.MAX_CONTENT_LENGTH
-
-
-
-
-from .DictPersistJSON import DictPersistJSON
-from .ClockUtils import check_ntp, ntp_servers
-from .IpUtils import mac, dhcp, address, netmask, gateway
-
-''' User settings are cached in an external file 'settings.json'.  Import
-	the Settings.py module and pass it through this class constructor to
-	update the default values below.  Simply delete the settings.json file
-	to return values to their factory defaults.
-
-	Access USER_SETTINGS like this:
-
-		from .common import Config
-		user_setting = Config.USER_SETTINGS['user_setting_key']
-
-	*Note: if you want to save a USER_SETTING that's part of
-	a dict itself (e.g., GENERAL > NAME), then you have to 
-	write the enire object.
-
-	I.e., do this to make changes that get saved to disk:
-
-		general = Config.USER_SETTINGS['GENERAL']
-		general['NAME'] = 'My cool new name'
-		Config.USER_SETTINGS['GENERAL'] = general
-
-
-	*Note: Use an underscore character on items that you DON'T
-	want to get serialized (e.g., to API respones).  For example
-	the username and passhash are user settings, but we never
-	want to serve them up as part of a response, so we prefix
-	with underscores.
-	'''
-USER_SETTINGS = DictPersistJSON(PATHS.SETTINGS, {
-
-	'__username': API.USERNAME,
-	'__passhash': API.PASSHASH,
-	'__device': INFO.DEVICE,
-
-	'general': {
-		'name': 'CME',
-		'description': 'Core monitoring engine',
-		'location': '',
-		'sitecode': ''
-	},
-
-	'support': {
-		'contact': '',
-		'email': '',
-		'phone': ''
-	},
-
-	'temperature': {
-		'displayUnits': TemperatureUnits.CELSIUS,
-		'warningTemp': 65, # ºC
-		'alarmTemp': 80 # ºC
-	},
-
-	'clock': {
-		'ntp': check_ntp(),
-		'servers': ntp_servers(),
-		'status': [],
-		'zone': 0,
-		'displayRelativeTo': RelativeTo.UTC,
-		'display12Hour': False,
-		'displayDateFormat': "YYYY-MM-DD",
-		'displayTimeFormat24Hour': "HH:mm:ss",
-		'displayTimeFormat12Hour': "h:mm:ss A"
-	},
-
-	'network': {
-		'mac': mac(),
-		'dhcp': dhcp(),
-		'address': address(),
-		'netmask': netmask(),
-		'gateway': gateway(),
-		'primary': '8.8.4.4',
-		'secondary': '8.8.8.8'
-	}
-})
-
-
 
 
